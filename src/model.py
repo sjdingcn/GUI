cdf5a3e34957b33a3c4eab3e2b911ec171c31807
sjdingcn <sd1051@rutgers.edu>
2019-11-26 12:50:37 -0500

    update file picker

--- src/model.py
+++ src/model.py
@@ -1,30 +1,12 @@
-import os
-# import sys
-#
-# from PyQt5 import uic
-# from PyQt5.QtWidgets import QApplication, QWidget
+from src.view.modelSenseController import Ui_modelMainWindow
+from PyQt5 import uic, QtWidgets
 
-#
-#
-#
-# if __name__ == '__main__':
-#     app = QApplication(sys.argv)
-#     ui_path = os.path.dirname(os.path.abspath(__file__))
-#     Form, Window = uic.loadUi(os.path.join(ui_path, "view/model.ui"))
-#     Window.show()
-#
-#     sys.exit(app.exec_())
 
-from PyQt5 import QtWidgets, uic
-import PyQt5.QtWebEngineWidgets
-
-import sys
-
-app = QtWidgets.QApplication([])
-ui_path = os.path.dirname(os.path.abspath(__file__))
-# Form, Window = uic.loadUi(os.path.join(ui_path, "view/model.ui"))
-win = uic.loadUi(os.path.join(ui_path, "view/model.ui")) # specify the location of your .ui file
-
-win.show()
-
-sys.exit(app.exec())
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    modelMainWindow = QtWidgets.QMainWindow()
+    ui = Ui_modelMainWindow()
+    ui.setupUi(modelMainWindow)
+    modelMainWindow.show()
+    sys.exit(app.exec_())
